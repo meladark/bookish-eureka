da8d4afdbf817ad98f4cfb01d459896a2bea7605
Mike V <mi.volkov@bi.zone>
2025-06-28 18:37:34 +0300

    HW11 Done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,60 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+	if flag.NArg() != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go-telnet [--timeout=10s] host port")
+		os.Exit(1)
+	}
+	address := flag.Arg(0) + ":" + flag.Arg(1)
+	client := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		fmt.Fprintln(os.Stderr, "...Connection error:", err)
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stderr, "...Connected to", address)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	done := make(chan struct{})
+	go func() {
+		if err := client.Receive(); err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+			} else {
+				fmt.Fprintln(os.Stderr, "...Receive error:", err)
+			}
+			close(done)
+		}
+	}()
+	go func() {
+		err := client.Send()
+		if err != nil {
+			if err.Error() == "connection closed by peer" {
+				fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+			} else {
+				fmt.Fprintln(os.Stderr, "...Send error:", err)
+			}
+		} else {
+			fmt.Fprintln(os.Stderr, "...EOF")
+		}
+		close(done)
+	}()
+	select {
+	case <-sigCh:
+		fmt.Fprintln(os.Stderr, "...SIGINT")
+	case <-done:
+	}
+	client.Close()
 }
--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -63,3 +63,38 @@ func TestTelnetClient(t *testing.T) {
 		wg.Wait()
 	})
 }
+
+func TestTelnetClient_Errors(t *testing.T) {
+	t.Run("connect timeout error", func(t *testing.T) {
+		in := &bytes.Buffer{}
+		out := &bytes.Buffer{}
+		timeout, _ := time.ParseDuration("10ms")
+		client := NewTelnetClient("192.0.2.1:65000", timeout, io.NopCloser(in), out)
+		err := client.Connect()
+		require.Error(t, err)
+	})
+
+	t.Run("close when no connection", func(t *testing.T) {
+		in := &bytes.Buffer{}
+		out := &bytes.Buffer{}
+		timeout, _ := time.ParseDuration("10s")
+		client := NewTelnetClient("localhost:0", timeout, io.NopCloser(in), out)
+		err := client.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("send error on closed connection", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+		in := &bytes.Buffer{}
+		out := &bytes.Buffer{}
+		timeout, _ := time.ParseDuration("10s")
+		client := NewTelnetClient(l.Addr().String(), timeout, io.NopCloser(in), out)
+		require.NoError(t, client.Connect())
+		require.NoError(t, client.Close())
+		in.WriteString("test\n")
+		err = client.Send()
+		require.Error(t, err)
+	})
+}
